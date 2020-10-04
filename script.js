00909dd91f6818ece5792ecfd4bcd4e75e7fdfe3
Helton Augusto <heltontux@gmail.com>
2020-10-03 22:11:48 -0300

    Definição de movimentos: iniciarJogo

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ let box = 32;
 let snake = [];
 snake[0] ={ x: 8*box, y: 8*box}
 
+let direction = "right";
+
 function criarBG(){
     context.fillStyle = "lightgreen";
     context.fillRect(0, 0, 16*box, 16*box);
@@ -16,5 +18,23 @@ function criarCobrinha(){
     }
 }
 
-criarBG();
-criarCobrinha();
+function iniciarJogo(){
+    criarBG();
+    criarCobrinha();
+    
+    let snakeX = snake[0].x;
+    let snakeY = snake[0].y;
+
+    if(direction == "right") snakeX +=box;
+    if(direction == "left") snakeX -=box;
+    if(direction == "up") snakeY -=box;
+    if(direction == "down") snakeY +=box;
+
+    snake.pop();
+    
+    let newHead = { x: snakeX, y: snakeY}
+
+    snake.unshift(newHead);
+}
+
+let jogo = setInterval(iniciarJogo, 100);
