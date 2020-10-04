e43f9f8f5b0508fbfcc91084db70559966c2a378
Helton Augusto <heltontux@gmail.com>
2020-10-03 23:07:06 -0300

    Criando comida para cobrinha

--- script.js
+++ script.js
@@ -5,6 +5,10 @@ let snake = [];
 snake[0] ={ x: 8*box, y: 8*box}
 
 let direction = "right";
+let food = {
+    x: Math.floor(Math.random()*15+1)*box,
+    y: Math.floor(Math.random()*15+1)*box
+}
 
 function criarBG(){
     context.fillStyle = "lightgreen";
@@ -16,6 +20,11 @@ function criarCobrinha(){
         context.fillStyle = "green";
         context.fillRect(snake[i].x, snake[i].y, box, box);
     }
+}
+
+function drawFood(){
+    context.fillStyle = "red";
+    context.fillRect(food.x, food.y, box, box);
 }
 
 document.addEventListener('keydown', update);
@@ -35,6 +44,7 @@ function iniciarJogo(){
 
     criarBG();
     criarCobrinha();
+    drawFood();
     
     let snakeX = snake[0].x;
     let snakeY = snake[0].y;
